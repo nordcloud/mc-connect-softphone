9f7ced9ff7f8e09b2ced93b0a504e37a49c3a724
Martin Petrov <martin.petrov@nordcloud.com>
2023-03-29 13:36:41 +0300

    show spinner when putting callerid

--- src/assets/homepage.js
+++ src/assets/homepage.js
@@ -7,8 +7,15 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 function init() {
-  const callerIdSelect = document.getElementById('caller-id-select');
   const ccpContainer = document.getElementById('ccp-container');
+  const callerIdSelect = document.getElementById('caller-id-select');
+  const callerIdLoader = document.getElementById('caller-id-loader');
+  const callerIdFieldset = document.querySelector('#caller-id-form fieldset');
+  const callerIdCheckmark = document.getElementById('caller-id-checkmark');
+
+  callerIdSelect.addEventListener('change', (event) => {
+    putCallerId(event.target.value);
+  });
 
   connect.core.initCCP(ccpContainer, {
     ccpUrl: AWS_CONNECT_URL,
@@ -18,10 +25,18 @@ function init() {
     },
   });
 
-  callerIdSelect.addEventListener('change', (event) => {
-    fetch('/caller-id', {
+  async function putCallerId(callerId) {
+    callerIdFieldset.disabled = true;
+    callerIdLoader.hidden = false;
+    callerIdCheckmark.hidden = true;
+
+    await fetch('/caller-id', {
       method: 'put',
-      body: event.target.value,
+      body: callerId,
     });
-  });
+
+    callerIdFieldset.disabled = false;
+    callerIdLoader.hidden = true;
+    callerIdCheckmark.hidden = false;
+  }
 }
